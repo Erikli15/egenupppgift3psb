2b20f5cbc077ae7e3438ce76dda4c865573b4ede
Erikli15 <erik.lindner@workmail.com>
2024-08-13 13:09:39 +0200

    kommenterat ut mysql

--- api/database/db.js
+++ api/database/db.js
@@ -2,22 +2,22 @@ const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  connectionLimit: 10,
-});
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   port: Number(process.env.DB_PORT),
+//   user: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   connectionLimit: 10,
+// });
 
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error("Error getting connection:", err);
-  } else {
-    console.log("Connected to database!");
-    connection.release();
-  }
-});
-
+// pool.getConnection((err, connection) => {
+//   if (err) {
+//     console.error("Error getting connection:", err);
+//   } else {
+//     console.log("Connected to database!");
+//     connection.release();
+//   }
+// });
+const pool = {};
 module.exports = { pool };
